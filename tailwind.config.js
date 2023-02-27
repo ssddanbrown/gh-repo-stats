250d92adfacc783babb8b38ca448bef81e62dcce
Dan Brown <ssddanbrown@googlemail.com>
2023-02-27 15:11:21 +0000

    Updated npm packages in use

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['public/index.html'],
+    content: ['public/index.html'],
 
     theme: {
         screens: {
